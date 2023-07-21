69890a035263114cbf411334fec4cdae2745c414
Artemzaicew <122361190+Artemzaicew@users.noreply.github.com>
2023-07-21 13:47:40 +0300

    add Statistics and Profile

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css'
+
+const Statistics = ({title, stats}) => {
+    return(
+        <section className={css.statistics}>
+  <h2 className={css.title}>{title}</h2>
+
+  <ul className="stat-list">
+  {stats.map(({ id, label, percentage }) => (
+          <li
+            className={css.item}
+            key={id}
+            
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+    
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+      PropTypes.exact({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+      }).isRequired
+    ).isRequired,
+  };
+
+  export default Statistics;
